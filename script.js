d126b28d638c0e1dc08c05c384000b16a75624b4
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 08:43:37 +0700

    ?

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ window.addEventListener('scroll', reveal);
         }
       }
     }
+window.addEventListener('scroll', eveal);
+
+    function eveal(){
+      var reveals = document.querySelectorAll('.eveal');
+
+      for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+          reveals[i].classList.add('active');
+        }
+        else{
+          reveals[i].classList.remove('active');
+        }
+      }
+    }
